fa14c3ec0664221534eea653729bb3ac8cd2b848
sinekumquat <verdafolio@gmail.com>
2015-04-10 13:26:05 -0400

    added cyber-

--- content_script.js
+++ content_script.js
@@ -1,2 +1,2 @@
-var replaced = $("body").html().replace(/cyber/g,'<span class=\"rainbow\">wizard</span>').replace(/Cyber /g,'<span class=\"rainbow\">Wizard </span>');
+var replaced = $("body").html().replace(/cyber/g,'<span class=\"rainbow\">wizard</span>').replace(/Cyber /g,'<span class=\"rainbow\">Wizard </span>').replace(/Cyber-/g,'<span class=\"rainbow\">Wizard-</span>');;
 $("body").html(replaced);
