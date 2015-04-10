064ca3b92a0f6fadef518476aa6946afd2e88678
Will Brand <will.brand@gmail.com>
2015-04-10 14:35:34 -0400

    Replace text nodes only

--- content_script.js
+++ content_script.js
@@ -1,2 +1,10 @@
-var replaced = $("body").html().replace(/cyber/g,'<span class=\"rainbow\">wizard</span>').replace(/Cyber /g,'<span class=\"rainbow\">Wizard </span>').replace(/Cyber-/g,'<span class=\"rainbow\">Wizard-</span>');;
-$("body").html(replaced);
+$('body').find('*').contents().filter(function() { return (this.nodeType === 3) }).each(function(i, elm) {
+  if(this.textContent.match(/(cyber)|(Cyber\-)|(Cyber )/g)) {
+    this.textContent = this.textContent
+      .replace(/cyber/g, '<span class=\"rainbow\">wizard</span>')
+      .replace(/Cyber /g, '<span class=\"rainbow\">Wizard </span>')
+      .replace(/Cyber-/g, '<span class=\"rainbow\">Wizard-</span>');
+
+    this.parentNode.replaceChild($('<span>'+this.textContent+'</span>')[0], this)
+  }
+});
